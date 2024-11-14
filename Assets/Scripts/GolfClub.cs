70e3a672a9886453d9430ce3a270d01334795d07
Josjed1 <152343882+Josjed1@users.noreply.github.com>
2024-11-14 14:40:17 -0600

    Pushing GolfClub Assets

---
+++ Assets/Scripts/GolfClub.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfClub : MonoBehaviour
+{
+	public Transform clubTransform;
+	public Transform gripTransform;
+	public float swingSpeed = 5f;
+	public float swingRange = 45f;
+	private Vector3 initialGripPosition;
+	private Vector3 initialClubRotation;
+	private bool isHovering = false;
+
+
+	private void Start()
+	{
+		initialClubRotation = clubTransform.rotation.eulerAngles;
+		initialGripPosition = gripTransform.position;
+
+	}	
+
+
+	private void Update()
+	{
+		if (isHovering)
+		{
+			Vector3 mousePosition = Input.mousePosition;
+			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePosition);
+
+			Vector3 direction = worldMousePos - gripTransform.position;
+
+			direction.Normalize();
+			
+			float swingAmount = direction.x * swingRange;
+
+			Quaternion newRotation = Quaternion.Euler(initialClubRotation.x + swingAmount, initialClubRotation.y, initialClubRotation.z);
+
+			clubTransform.rotation = newRotation;
+		}
+	}
+	private void OnMouseEnter()
+	{
+		isHovering = true;
+	}
+
+	private void OnMouseExit()
+	{
+		isHovering = false;
+	}
+}
